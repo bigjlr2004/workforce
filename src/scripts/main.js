13a38ef1a33cf73a12787feaac4614c922b89336
John Roberts <bigjlr2004@yahoo.com>
2023-01-04 13:41:14 -0600

    Finished Customer List with matching employees

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,26 +1,21 @@
 import { fetchData } from "./dataAccess.js"
-import {employee} from "./employeeList.js"
+import { workforce } from "./workforce.js"
 
 
 
-/* define a function that will invoke the function from the penpal.js module
-    The following function invokes the fetchData function imported from the dataAccess module
-    For each of the databases in the json-server database we need to fetch the data before we can use it 
-    in our application. When fetchData() is invoked the database needed is passed along to it in quotes inside
-    parentheses. By having one function called fetchData and passing a parameter as an argument we 
-    cut back on the repetitive code in the application.
-*/
+
+
 const mainContainer = document.querySelector("#container")
 const render =() => {
     
     fetchData("departments").then(
     fetchData("locations")).then(() =>
-    fetchData("employeeCustomers")).then(() =>
+    fetchData("customerEmployees")).then(() =>
     fetchData("customers")).then(() =>
     fetchData("computers")).then(() =>
     fetchData("employees")).then(()=>
     {
-            mainContainer.innerHTML = employee()
+            mainContainer.innerHTML = workforce()
         }
     )
 }
